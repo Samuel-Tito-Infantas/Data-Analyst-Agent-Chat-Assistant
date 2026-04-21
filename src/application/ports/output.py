b825752b529e929e3960e39ec35d4c095638db98
plagueis <samuel347@gmail.com>
2026-04-21 12:12:10 -0300

    update: new template ports Refactory

---
+++ src/application/ports/output.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from domain.entities import RetrievedContext, AgentResponse, KnowledgeDocument
+
+
+class TextEmbedderRepo(ABC):
+    @abstractmethod
+    def embed_text(self, text: str) -> List[float]:
+        pass
+
+class VectorDatabaseRepo(ABC):
+    @abstractmethod
+    def search_by_vector(self, query_vector: List[float], top_k: int = 3) -> List[RetrievedContext]:
+        pass
+
+class InsertTextDatabaseRepo(ABC):
+    @abstractmethod
+    def add_text_database(self, texts:List[KnowledgeDocument]):
+        pass
+
+class LLMServiceRepo(ABC):
+    @abstractmethod
+    def generate_grounded_response(self, question: str, context: AgentResponse) -> str:
+        pass
+
